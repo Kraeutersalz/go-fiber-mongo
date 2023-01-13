8e6c4caa61b6076844dce1a942cb326e6c43f5f0
Patrick Jarren <Patrick93@hotmail.de>
2023-01-13 22:15:44 +0100

    setting up go with fiber and mongodb

--- main.go
+++ main.go
@@ -1,13 +1,61 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+
+	"github.com/Kraeutersalz/go-fiber-mongo/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func main() {
+	//init app
+	err := initApp()
+	if err != nil {
+		panic(err)
+	}
+
+	//defer close database
+	defer database.CloseMongoDB()
+
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Post("/", func(c *fiber.Ctx) error {
+		//write a title to database
+		sampleSong := bson.M{"name": "Sample Song"}
+		collection := database.GetCollection("Songs")
+		nDoc, err := collection.InsertOne(context.TODO(), sampleSong)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Error inserting Sond")
+		}
+
+		//send down info about song
+		return c.JSON(nDoc)
 	})
 
 	app.Listen(":3000")
+}
+
+func initApp() error {
+	//setup ENV
+	err := LoadENV()
+	if err != nil {
+		return err
+	}
+
+	//setup Database
+	err = database.StartMongoDB()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadENV() error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+	return nil
 }
